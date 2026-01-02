2ea01dcf9635d0f492e2f6a29c53c0b4f68569bd
deividas2026 <deividasaldo@protonmail.com>
2026-01-02 18:23:15 +0200

    add week1 mario solutions

---
+++ week1/mario-less/mario.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+    int height = 0;
+    int spaces = 0;
+
+    while (height < 1)
+    {
+        height = get_int("Height: ");
+    }
+
+    for (int i = 1; i <= height; i++)
+    {
+        spaces = height - i;
+        for (int j = 0; j < spaces; j++) printf(" ");
+        for (int k = 0; k < i; k++) printf("#");
+        printf("\n");
+    }
+    return 0;
+}
+
