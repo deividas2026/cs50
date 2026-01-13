92d19917554a3171e13d1bb16eba31331fb8318f
deividas2026 <deividasaldo@protonmail.com>
2026-01-13 20:50:22 +0200

    add filter-less/grayscale

---
+++ week4/filter-less/helpers.c
@@ -0,0 +1,37 @@
+#include "helpers.h"
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE pixel = image[i][j];
+            BYTE gray = (pixel.rgbtRed + pixel.rgbtGreen + pixel.rgbtBlue) / 3;
+            image[i][j].rgbtRed = gray;
+            image[i][j].rgbtGreen = gray;
+            image[i][j].rgbtBlue = gray;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
